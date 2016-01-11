c9873f047fa490c94bd8ae9e04c8e51bce32ac8b
pcarrara21 <p.carrara21@gmail.com>
2016-01-11 16:30:05 +0100

    main changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
-(function() {
-  this.onHover = function(id) {
-    switch (id) {
-      case 'circle1':
-        $('#github_img').attr('src', 'assets/github.png');
-        document.getElementById('github').style.visibility = 'visible';
-        break;
-      case 'circle2':
-        $('#mail_img').attr('src', 'assets/github.png');
-        document.getElementById('mail').style.visibility = 'visible';
-        break;
-      case 'circle3':
-        $('#linkedin_img').attr('src', 'assets/github.png');
-        document.getElementById('linkedin').style.visibility = 'visible';
+function onHover(id){
+    switch(id){
+        case "circle1":
+            $("#github_img").attr('src', 'assets/git1.png');
+            document.getElementById("github").style.visibility = 'visible';
+            break;
+        case "circle2":
+            $("#mail_img").attr('src', 'assets/mail1.png');
+            document.getElementById("mail").style.visibility = 'visible';
+            break;
+        case "circle3":
+            $("#linkedin_img").attr('src', 'assets/linkedin1.png');
+            document.getElementById("linkedin").style.visibility = 'visible';
+            break;
     }
-  };
+}
 
-  this.onOut = function(id) {
-    switch (id) {
-      case 'circle1':
-        $('#github_img').attr('src', 'assets/git1.png');
-        document.getElementById('github').style.visibility = 'hidden';
-        break;
-      case 'circle2':
-        $('#mail_img').attr('src', 'assets/mail.png');
-        document.getElementById('mail').style.visibility = 'hidden';
-        break;
-      case 'circle3':
-        $('#linkedin_img').attr('src', 'assets/linkedin.png');
-        document.getElementById('linkedin').style.visibility = 'hidden';
+function onOut(id){
+    switch(id){
+        case "circle1":
+            $("#github_img").attr('src', 'assets/git.png');
+            document.getElementById("github").style.visibility = 'hidden';
+            break;
+        case "circle2":
+            $("#mail_img").attr('src', 'assets/mail.png');
+            document.getElementById("mail").style.visibility = 'hidden';
+            break;
+        case "circle3":
+            $("#linkedin_img").attr('src', 'assets/linkedin.png');
+            document.getElementById("linkedin").style.visibility = 'hidden';
+            break;
     }
-  };
-
-}).call(this);
+}
