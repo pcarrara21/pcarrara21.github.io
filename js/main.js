b27a6765056890509a9f64bd09a3206b97972db6
PCAR <username@users.noreply.github.com>
2017-12-29 19:02:34 +0100

    Personal page revision

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ function onHover(id){
             document.getElementById("github").style.visibility = 'visible';
             break;
         case "circle2":
-            $("#mail_img").attr('src', 'assets/mail1.png');
-            document.getElementById("mail").style.visibility = 'visible';
+            $("#salesforce_img").attr('src', 'assets/salesforce1.png');
+            document.getElementById("salesforce").style.visibility = 'visible';
             break;
         case "circle3":
             $("#linkedin_img").attr('src', 'assets/linkedin1.png');
@@ -22,8 +22,8 @@ function onOut(id){
             document.getElementById("github").style.visibility = 'hidden';
             break;
         case "circle2":
-            $("#mail_img").attr('src', 'assets/mail.png');
-            document.getElementById("mail").style.visibility = 'hidden';
+            $("#salesforce_img").attr('src', 'assets/salesforce.png');
+            document.getElementById("salesforce").style.visibility = 'hidden';
             break;
         case "circle3":
             $("#linkedin_img").attr('src', 'assets/linkedin.png');
