c54394d1316a2201cf0f808e1b5e36d42cec76d7
Paolo Carrara <p.carrara21@gmail.com>
2020-12-08 15:56:23 +0100

    Complete page redesign

--- js/main.js
+++ js/main.js
@@ -1,3 +1,81 @@
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+
+  TxtRotate.prototype.tick = function() {
+
+    let i = this.loopNum % this.toRotate.length;
+
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting)
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    else
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    let that = this;
+    let delta = 180 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+
+        delta = this.period;
+        this.isDeleting = true;
+
+    } else if (this.isDeleting && this.txt === '') {
+
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    let stop = (this.loopNum == (this.toRotate.length -1));
+
+    if(!(stop && this.isDeleting)){
+        setTimeout(function(){that.tick();}, delta);
+    }
+  };
+
+  window.onload = function() {
+
+    var elements = document.getElementsByClassName('txt-rotate');
+
+    for (var i = 0; i < elements.length; i++){
+
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+
+        if(toRotate)
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+
+    //Would have used Fetch API b
+    fetch('./assets/files/quotes.txt')
+        .then(response => console.log(response));
+
+  };
+
+  /*function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}*/
+
+
 function onHover(id){
     switch(id){
         case "circle1":
