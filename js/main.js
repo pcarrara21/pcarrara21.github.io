7aeef54b329fa4907a122a7bb9b361820b410505
Paolo Carrara <p.carrara21@gmail.com>
2020-12-14 23:09:23 +0100

    General refactoring

--- js/main.js
+++ js/main.js
@@ -65,56 +65,22 @@ var TxtRotate = function(el, toRotate, period) {
 function loadQuoteOfTheDay(){
 
     fetch('./assets/files/quotes.txt')
-        .then(response => response.text().then(data => {
+    .then(response => response.text().then(data => {
 
-            let quote;
-            let nowDate = new Date();
-            let startOfYear = new Date(nowDate.getFullYear(), 0 ,0);
-            let quoteNum = Math.trunc(((nowDate - startOfYear) / (1000*60*60*24)) % 50);
+        let quote;
+        let nowDate = new Date();
+        let startOfYear = new Date(nowDate.getFullYear(), 0 ,0);
+        let quoteNum = Math.trunc(((nowDate - startOfYear) / (1000*60*60*24)) % 50);
 
-            if(quoteNum == 50)
-                quote = data.substring(data.indexOf('#50') + 3);
-            else
-                quote = data.substring(data.indexOf('#' + quoteNum) + 1 + quoteNum.toString().length, data.indexOf('#' + (quoteNum + 1)));
+        if(quoteNum == 50)
+            quote = data.substring(data.indexOf('#50') + 3);
+        else
+            quote = data.substring(data.indexOf('#' + quoteNum) + 1 + quoteNum.toString().length, data.indexOf('#' + (quoteNum + 1)));
 
-            document.getElementById('quote').innerHTML = quote;
-        }))
-        .catch((error) => {
-            console.error('Error retrieving quotes file:', error);
-          });
+        document.getElementById('quote').innerHTML = quote;
+    }))
+    .catch((error) => {
+        console.error('Error retrieving quotes file:', error);
+    });
 
-}
-
-function onHover(id){
-    switch(id){
-        case "circle1":
-            $("#github_img").attr('src', 'assets/git1.png');
-            document.getElementById("github").style.visibility = 'visible';
-            break;
-        case "circle2":
-            $("#salesforce_img").attr('src', 'assets/salesforce1.png');
-            document.getElementById("salesforce").style.visibility = 'visible';
-            break;
-        case "circle3":
-            $("#linkedin_img").attr('src', 'assets/linkedin1.png');
-            document.getElementById("linkedin").style.visibility = 'visible';
-            break;
-    }
-}
-
-function onOut(id){
-    switch(id){
-        case "circle1":
-            $("#github_img").attr('src', 'assets/git.png');
-            document.getElementById("github").style.visibility = 'hidden';
-            break;
-        case "circle2":
-            $("#salesforce_img").attr('src', 'assets/salesforce.png');
-            document.getElementById("salesforce").style.visibility = 'hidden';
-            break;
-        case "circle3":
-            $("#linkedin_img").attr('src', 'assets/linkedin.png');
-            document.getElementById("linkedin").style.visibility = 'hidden';
-            break;
-    }
 }
