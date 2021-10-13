98f5cb7dd37ddb6eae1480b58b8dd906cd7ea5ae
Paolo Carrara <p.carrara21@gmail.com>
2021-10-13 23:22:58 +0200

    Site improvement

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 class TxtAnimate{
 
-    constructor(el, toAnimate, period){
+    constructor(el, toAnimate, period, sequence){
         this.toAnimate = toAnimate;
         this.el = el;
         this.loopNum = 0;
         this.period = parseInt(period, 10) || 1000;
         this.txt = '';
         this.isDeleting = false;
+        this.sequence = sequence;
     }
 
     tick(){
@@ -20,7 +21,7 @@ class TxtAnimate{
         else
             this.txt = fullTxt.substring(0, this.txt.length + 1);
 
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        this.el.innerHTML = '<span id="typed" class="wrap">' + this.txt + '</span>';
 
         let that = this;
         let delta = 180 - Math.random() * 100;
@@ -49,13 +50,33 @@ class TxtAnimate{
 
 function init(){
 
-    var element = document.getElementsByClassName('txt-animate')[0];
+    var sequence = (Math.random() + 1).toString(36).substring(5);
+    document.title += ' - ' + sequence;
 
+    var element = document.getElementsByClassName('txt-animate')[0];
     var toAnimate = element.getAttribute('data-animate');
     var period = element.getAttribute('data-period');
 
-    txtAnimation = new TxtAnimate(element, JSON.parse(toAnimate), period);
+    txtAnimation = new TxtAnimate(element, JSON.parse(toAnimate), period, sequence);
     txtAnimation.tick();
+
+    document.addEventListener("keypress", function(event){
+        var typed = document.getElementById('typed');
+        var charCode = event.keyCode || event.which;
+        var charStr = String.fromCharCode(charCode);
+        if(charCode != 13)
+            typed.innerText += charStr;
+    });
+
+    document.addEventListener("keydown", function(event){
+        var typed = document.getElementById('typed');
+        var charCode = event.keyCode || event.which;
+        if(charCode == 8)
+            typed.innerText = typed.innerText.substring(0, typed.innerText.length - 1);
+        if(charCode == 13 && typed.innerText === sequence)
+            makeGithubFall();
+    });
+
 
 }
 
@@ -79,7 +100,33 @@ function loadQuoteOfTheDay(){
     .catch((error) => {
         console.error('Error retrieving quotes file:', error);
     });
+}
 
+function makeGithubFall(){
+    let github = document.getElementById('github');
+    github.className += ' github_fall';
+    setTimeout(revealAdditionalInfo, 4000);
+}
+
+function revealAdditionalInfo(){
+    let additionalInfo = document.getElementById('additionalInfo');
+    let info = 'Damn, this site is falling to pieces..</br></br> anyway, since you\'re here ' +
+                'you might be interested in knowing something more about myself.';
+    let info1 = 'As you can guess I love easter eggs, puzzles, and board games and ' +
+                'I think that playing itself it\'s an essential activity in everyday life.';
+    let info2 = 'Playing has been the start of human evolution and it\'s a notable feat even in animals: ' +
+                'by playing we discover our strenghts and weaknesses and we learn coordination, logic and rational thinking.';
+    let info3 = 'The best team is not the one made with the best players, but it\'s the one with the players ' +
+                'that know best how to play together.';
+    let info4 = 'So if you have arrived here congratulations, here\'s your flag!';
+    additionalInfo.style.display = 'block';
+    additionalInfo.style.position = 'absolute';
+    additionalInfo.style.top = window.outerHeight + 'px';
+    additionalInfo.innerHTML = '<p class="content">' + info + '</p><br/><br/>' +
+    '<p class="content">' + info1 + '</p><br/><br/>' +
+    '<p class="content">' + info2 + '</p><br/><br/>' +
+    '<p class="content"><b/>' + info3 + '</b></p><br/><br/>' +
+    '<p class="content">' + info4 + '</p><br/><br/>';
 }
 
 window.onload = function() {
@@ -87,5 +134,4 @@ window.onload = function() {
     init();
 
     loadQuoteOfTheDay();
-
 };
